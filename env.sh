38c0fea34ae43f69c44f51f10f7894505766d609
AndrewCopeland <andcope1995@gmail.com>
2020-01-27 11:53:31 -0500

    parameterize newly created unix user

--- env.sh
+++ env.sh
@@ -1,6 +1,5 @@
-
 export CONJUR_APPLIANCE_URL=https://conjur-master
 export CONJUR_ACCOUNT=conjur
 export CONJUR_CERT_FILE=conjur-conjur.pem
 export CONJUR_AUTHN_LOGIN=host/ansible1
-export CONJUR_AUTHN_API_KEY=abksj8vn9jzm3t70nmfhcp7k1w0h59f281c5ca2c4y2nt3t5wmky
+export CONJUR_AUTHN_API_KEY=3ht6g351zc51ez1fgw1hs3hk0d8f2mxcmev3dxj0fk2gsw5db1rdgp9v
